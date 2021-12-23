e422c0b3612c6f7118c657dbf19e1d5bbe82a4b9
anilraj <anil050797@gmial.com>
2021-12-23 14:05:41 +0530

    heading done

--- src/containers/CategoryAccordion/Accordion.js
+++ src/containers/CategoryAccordion/Accordion.js
@@ -10,14 +10,13 @@ import './accordion.css';
 export default class ProfileMainBody extends Component {
   render() {
     return ( 
-      <div className="container mt-5" style={{ maxWidth: '500px', background: '' }}>
-        <div className="row">
-          <div className="col">
+      <div className=" mt-5" style={{ maxWidth: '500px', background: '' }}>
+        
             <div class="accordion accordion-flush" id="accordionFlushExample" style={{ color: '#fff !important' }}>
               <div class="accordion-item mb-3">
                 <h2 class="accordion-header" id="flush-headingOne">
                   <button class="pb-2 acc-btn ct-heading accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne" style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)'}}>
-                    BestSellers (9)
+                    Best Sellers (9)
                   </button>
                 </h2>
                 <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
@@ -34,7 +33,7 @@ export default class ProfileMainBody extends Component {
               <div class="accordion-item mb-3">
                 <h2 class="accordion-header" id="flush-headingTwo">
                   <button class="pb-2 acc-btn ct-heading accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo"  style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)'}}>
-                    cHINES (5)
+                    Chines (5)
                   </button>
                 </h2>
                 <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
