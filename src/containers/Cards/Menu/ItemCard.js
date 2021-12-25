b855979a468e87f12f8befad9d63ca4443627ac0
Akshat <akshat@sysadmins-MacBook-Pro.local>
2021-12-25 14:51:35 +0530

    differ veg non veg

--- src/containers/Cards/Menu/ItemCard.js
+++ src/containers/Cards/Menu/ItemCard.js
@@ -4,12 +4,13 @@ import AddItem from '../../Buttons/AddItem';
 
 class MenuItemCard extends Component {
     render() {
+        const dietary_flag_color = this.props.dietary_flag ==='VEG' ? 'green' : 'red';
         return (
             <div className=" mt-2" style={{ maxWidth: '500px'}}>
             <div className="container">
                 <div className="row">
                     <div className="col-8 ">
-                        <span style={{ color: ' #ee9c00', fontSize: '12px' }}><i className="far fa-stop-circle dot " style={{ color: ' rgb(46, 44, 44)', fontSize: '15px' }}></i> &nbsp; <i className="fas fa-star rating-stat" ></i> Bestseller</span>
+                        <span style={{ color: ' #ee9c00', fontSize: '12px' }}><i className="far fa-stop-circle dot " style={{color: dietary_flag_color, fontSize: '15px' }}></i> &nbsp; <i className="fas fa-star rating-stat" ></i> Bestseller</span>
                         <h6 className="pt-2" style={{ color: ' #282c3f', fontWeight: '500' }}>{this.props.item_name}</h6>
                         <span className="mt-n-2" style={{ fontSize: '12px' }}>{this.props.item_price}</span>
                     </div>
