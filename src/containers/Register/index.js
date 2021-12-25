4c3e54b450498e83d67a832ed25f110254761452
anilraj <anil050797@gmial.com>
2021-12-25 15:14:10 +0530

    serach bar icon added

--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -7,12 +7,14 @@ import { Link } from 'react-router-dom';
 class Register extends Component {
     render() {
         return (
+            
+                
             <div className='container' style={{maxWidth: '500px', marginBottom:'150px', height: '100vh'}}>
-                <NavBar />
-                <div className="row" style={{marginTop: '50vh'}}>
+            <NavBar />
+                <div className="row" style={{marginTop:'50%'}}>
                     <div className="col" >
                         <section>
-                            <div className="pb-4" style={{ background: '#fff'}}>
+                            <div className="pb-4">
                                 <div className="row my-2">
                                     <div className="col">
                                         <Link to="/login">
@@ -29,7 +31,8 @@ class Register extends Component {
                         </section>
                     </div>
                 </div>
-            </div>
+                </div>
+           
         )
     }
 }
