eae8d2d7c71aa8ebebda7401ed2264f605b8edee
anilraj <anil050797@gmial.com>
2022-01-04 13:14:01 +0530

    lang added 0.8

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import './containers/CategoryAccordion/accordion.css'
 
 //import CartBody from './components/cart/CartBody';
 
+
 class App extends Component {
 
   constructor(props){
--- src/index.js
+++ src/index.js
@@ -2,12 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {rootReducer} from './store/reducer';
+
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import App0 from './App0';
+
+
+i18n
+.use(initReactI18next) 
+.use(LanguageDetector)
+.use(HttpApi)
+.init({
+  supportedLngs: ['en', 'fr'],
+  fallbackLng: "en",
+  detection: {
+    order: ['path', 'cookie', 'htmlTag', 'localStorage',  'subdomain'],
+    cashes: ['cookie'],
+  },
+  backend: {
+    loadPath: 'assect/locales/{{lng}}/translation.json',
+  },
+  interpolation: {
+    escapeValue: false 
+  },
+  react: {useSuspense: false}
+});
+
 
 
 const logger = store =>{
@@ -24,6 +54,7 @@ const store = createStore(rootReducer, applyMiddleware(logger,thunk));
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <App0/>
       <App />
     </Provider>
   </React.StrictMode>,
