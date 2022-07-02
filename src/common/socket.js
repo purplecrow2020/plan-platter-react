e95a7094e4bab0a08dd0dbe6ef84427ed4b2c73a
Akshat <akshat@192.168.0.122>
2022-07-02 20:32:01 +0530

    socket endpoint point to prod

--- src/common/socket.js
+++ src/common/socket.js
@@ -1,7 +1,7 @@
 
 // import socketIOClient from 'socket.io-client';
-const socketEndpoint = "http://localhost:5000/";
-// const socketEndpoint = "http://3.91.180.25:5000/";
+// const socketEndpoint = "http://localhost:5000/";
+const socketEndpoint = "http://3.91.180.25:5000/";
 export default socketEndpoint;
 // export default socketIOClient(socketEndpoint, {
 //     query: `connected_user_type=user&vendor_id=${localStorage.getItem('vendor_id')}&auth=${localStorage.getItem('authKey')}&table_id=${localStorage.getItem('table_id')}`
