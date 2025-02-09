0e7cfad127bc4f8a36cb6e6cd8b4395fb31e53b4
akshat-dev-ad <akshat.jain2@allen.in>
2025-02-09 13:08:10 +0530

    firebase config

--- src/common/firebaseConfig.js
+++ src/common/firebaseConfig.js
@@ -1,23 +1,13 @@
 import firebase from 'firebase';
 
-// const firebaseConfig  = {
-//     apiKey: "AIzaSyClWLgbsV8ojshhcexsZ1mgnQE9f0UM7UI",
-//     authDomain: "biviolin-7959e.firebaseapp.com",
-//     projectId: "biviolin-7959e",
-//     storageBucket: "biviolin-7959e.appspot.com",
-//     messagingSenderId: "793832895294",
-//     appId: "1:793832895294:web:b7b680c0ef6a63084bb26f",
-//     measurementId: "G-HZS9WZEES5"
-//   };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBxAlDLX8TzIYch_GR7Jsmjtd195d7z8Vw",
-  authDomain: "plan-platter.firebaseapp.com",
-  projectId: "plan-platter",
-  storageBucket: "plan-platter.appspot.com",
-  messagingSenderId: "44319218709",
-  appId: "1:44319218709:web:eb84d82cc5d4d7d78c821c",
-  measurementId: "G-V6DVHND11J"
+  apiKey: "AIzaSyD7Bwpki9wZSuky0hW5e3PmhLuWR1hT1LI",
+  authDomain: "plan-n-platter.firebaseapp.com",
+  projectId: "plan-n-platter",
+  storageBucket: "plan-n-platter.firebasestorage.app",
+  messagingSenderId: "807690369273",
+  appId: "1:807690369273:web:6fbe4c8cd30e33392c0a28",
+  measurementId: "G-LK5RE0LNF5"
 };
 
 
